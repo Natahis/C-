029ac5c87aa16f049fec8c1cc22bc002756a2b5d
Natahis <natahis@bk.ru>
2022-08-08 20:15:04 +0300

    tast1

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Определить является ли число квадратом другого
+Console.WriteLine("Введите два целых числа:");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+if (A == B * B)
+{
+    Console.Write($"Да, число {A} является квадратом числа {B}");
+}
+else
+{
+    Console.Write($"Нет, число {A} не является квадратом числа {B}");
+}
