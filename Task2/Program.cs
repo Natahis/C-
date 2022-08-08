b6050e7ed85b2e34f9983d98794e1c89dacae401
Natahis <natahis@bk.ru>
2022-08-08 20:41:05 +0300

    tesk2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дни недели
+Console.WriteLine("Введите число от 1 до 7");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 1) Console.Write("Понедельник");
+else if (num == 2) Console.Write("Вторник");
+else if (num == 3) Console.Write("Среда");
+else if (num == 4) Console.Write("Четверг");
+else if (num == 5) Console.Write("Пятница");
+else if (num == 6) Console.Write("Суббота");
+else if (num == 7) Console.Write("Воскресенье");
+else  Console.Write("Неправильный диапозон чисел");
