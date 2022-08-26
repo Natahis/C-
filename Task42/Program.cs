62f5e100a005b22d084f316e7617fca86193a9cd
Natahis <natahis@bk.ru>
2022-08-26 19:40:37 +0300

    Tas5

--- Task42/Program.cs
+++ Task42/Program.cs
@@ -7,13 +7,21 @@
 Console.WriteLine("Введите целое пложительное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int result = 0;
-int mult=1;
+// int result = 0;
+// int mult=1;
 
-while (num > 0)
+// while (num > 0)
+// {
+//     result = result + num % 2 * mult;
+//     num = num / 2;
+//     mult = mult * 10;
+// }
+// Console.WriteLine(result);
+
+void DecToBin(int number)
 {
-    result = result + num % 2 * mult;
-    num = num / 2;
-    mult = mult * 10;
+    if (number == 0) return;
+    DecToBin(number / 2);
+    Console.Write(number % 2);
 }
-Console.WriteLine(result);
+DecToBin(num);
