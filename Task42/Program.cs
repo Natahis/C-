b09333189c41ca0f3c2829fe94192bd802f00b4e
Natahis <natahis@bk.ru>
2022-08-26 18:34:29 +0300

    Task42

---
+++ Task42/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите целое пложительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+int mult=1;
+
+while (num > 0)
+{
+    result = result + num % 2 * mult;
+    num = num / 2;
+    mult = mult * 10;
+}
+Console.WriteLine(result);
