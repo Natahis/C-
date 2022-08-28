4ef4e66f211fde2c589a65472175569c3f7f2d05
Natahis <natahis@bk.ru>
2022-08-28 14:01:10 +0300

    Task48

---
+++ Task48/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+int[,] GreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            // if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4},");
+            // else Console.Write($"{matrix[i, j], 4}");
+            Console.Write(j < matrix.GetLength(1) - 1?$"{matrix[i, j], 3},":$"{matrix[i, j], 3}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] arr2D = GreateMatrixRndInt(3, 4, -99, 99);
+PrintMatrix(arr2D);
